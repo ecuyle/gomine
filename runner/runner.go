5dde8b14451abc1cc90800569150d60db3f6a881
Eric Cuyle <ecuyle@gmail.com>
2020-12-17 01:14:19 -0800

    Adds function to get versions by ID

--- runner/runner.go
+++ runner/runner.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -28,31 +29,81 @@ type VersionManifest struct {
 	Versions []Version     `json:"versions"`
 }
 
-// GetVersionManifest returns the Mojang versions manifest
-func GetVersionManifest() *VersionManifest {
-	manifestURL := "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+// VersionDownload struc
+type VersionDownload struct {
+	Sha1 string `json:"sha1"`
+	Size int    `json:"size"`
+	URL  string `json:"url"`
+}
 
-	resp, err := http.Get(manifestURL)
+// VersionDetail struct
+type VersionDetail struct {
+	Downloads map[string]VersionDownload `json:"downloads"`
+	ID        string                     `json:"id"`
+}
+
+// GetVersionByID returns the Mojang version object given a version id
+func GetVersionByID(versionID string) (*Version, error) {
+	versionManifest, err := GetVersionManifest()
 
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
+	}
+
+	for i := 0; i < len(versionManifest.Versions); i++ {
+		var version = versionManifest.Versions[i]
+
+		if versionID == version.ID {
+			return &version, nil
+		}
+	}
+
+	return nil, errors.New("Could not find version with id: " + versionID)
+}
+
+// GetVersionManifest returns the Mojang versions manifest
+func GetVersionManifest() (*VersionManifest, error) {
+	manifestUrl := "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+
+	resp, err := http.Get(manifestUrl)
+
+	if err != nil {
+		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	var versionManifest VersionManifest
-	json.Unmarshal(body, &versionManifest)
+	err = json.Unmarshal(body, &versionManifest)
 
-	return &versionManifest
+	if err != nil {
+		return nil, err
+	}
+
+	return &versionManifest, nil
 }
 
 func main() {
-	versionManifest := GetVersionManifest()
+	versionManifest, err := GetVersionManifest()
 
-	log.Println(versionManifest.Latest.Release)
-	log.Println(versionManifest.Versions[0].URL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Latest Release:", versionManifest.Latest.Release)
+
+	versionID := "1.16.4"
+
+	version, err := GetVersionByID(versionID)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(version)
+	log.Println(version.URL)
 }
