4eefb8277f7cf4d817666460f250a8ae5186a07f
Eric Cuyle <ecuyle@gmail.com>
2023-08-18 00:45:06 +0200

    Adds -NotFound and -StatusOk responses in api utils

--- internal/api/utils.go
+++ internal/api/utils.go
@@ -15,3 +15,12 @@ func RespondWithInternalServerError(context *gin.Context, err error) {
 func RespondWithStatusCreated(context *gin.Context, data any) {
 	context.IndentedJSON(http.StatusCreated, data)
 }
+
+func RespondWithNotFound(context *gin.Context, err error) {
+	log.Println(err)
+	context.String(http.StatusNotFound, err.Error())
+}
+
+func RespondWithStatusOk(context *gin.Context, data any) {
+	context.IndentedJSON(http.StatusOK, data)
+}
